06b0a5c4cc631b0e1616967e96df9641079db7b1
ajcltm <ajcltm@gmail.com>
2021-06-17 01:20:20 +0900

    folling zipline

--- BackTestAlgo.py
+++ BackTestAlgo.py
@@ -1,18 +1,29 @@
+import pandas as pd
+import types
+
 class BackTester:
     def __init__(self, initialize, tradingAlgo):
         print('Hello backtesting')
-        self.context = {'dataColumn': ''}
+        self.context = types.SimpleNamespace()
         initialize(self.context)
+
+        self.portfolio = types.SimpleNamespace()
+        positions = pd.DataFrame(columns=[self.context.security], index=['amounts', 'type'])
+        self.portfolio.positions = positions
+        self.context.portfolio = self.portfolio
 
         self.tradingAlgo = tradingAlgo
 
     def run(self, data):
-        dataColumn = self.context['dataColumn']
-        for dayData in data[dataColumn]:
-            actionDict = self.tradingAlgo(dayData)
-            if actionDict['type'] == 'sell':
-                # 판다
-                print('sell')
-            elif actionDict['type'] == 'buy':
-                # 산다
-                print('buy')
+        resultColumns = ['date', 'portfolio_value']
+        self.result = pd.DataFrame(columns=resultColumns)
+        for i in range(0, len(data)):
+            dayData = data.iloc[i,:]
+            s = pd.Series([dayData.name, self.context.portfolio.positions[self.context.security].amounts*dayData[self.context.security]], index=resultColumns)
+            self.result = self.result.append(s, ignore_index=True)
+            self.tradingAlgo(self.context, dayData)
+
+        return self.result
+
+    def order(self, ticker, price, quantity) :
+        self.context.portfolio.positions[self.context.security].amounts += quantity
--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import pandas as pd
+import numpy as np
+import datetime
+import types
 from BackTestAlgo import *
 
 
-
 def my_init(context):
-    context['dataColumn'] = 'price'
+    context.security = 'NVDA'
+    context.hold = False
 
-def handle_data(dayData):
-    print(dayData)
-    actionDict = {'type': ''}
-    if dayData == 1000:
-        actionDict['type'] = 'buy'
-    if dayData == 2000:
-        actionDict['type'] = 'sell'
+def handle_data(context, dayData):
+    if dayData[context.security] > 0 :
+        context.hold = True
+        context.portfolio.positions[context.security].amounts = 1
+        print("long")
+    if dayData[context.security] == 2000:
+        context.hold = False
+        print("short")
 
-    return actionDict
-
+# def symbol(code):
+#     if code == 'AAPL':
+#         return 1000
+#     elif code == 'NVDA':
+#         return 1001
+#     elif code == 'SPY':
+#         return 1002
 
 if __name__ == '__main__':
 
     tester = BackTester(initialize=my_init, tradingAlgo=handle_data)
-
-    data = pd.DataFrame(data={'price': [1000, 2000]})
-    tester.run(data)
+    print(tester.context)
+    price_list = np.random.randint(650, 720, 10)
+    date_list = [datetime.datetime(2021, 1, i+1) for i in range(0, 10)]
+    data = pd.DataFrame(data={'NVDA': price_list}, index=date_list)
+    result = tester.run(data)
+    print(result)
